db7b7a8258c821d9299bbf8c259f23142afe3650
paperbab <fujunang_mail@163.com>
2023-03-05 15:26:39 +0800

    add

---
+++ rime.lua
@@ -0,0 +1,6 @@
+local easy_en = require("easy_en")
+time_translator = require("time")
+date_translator = require("date")
+week_translator = require("week")
+lunar_translator = require("lunar")
+easy_en_enhance_filter = easy_en.enhance_filter
